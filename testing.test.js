ac4de120186c360758558dabeab68968dbc41ea0
Anand-1812 <anand.work2005@gmail.com>
2025-06-18 18:58:09 +0530

    add reverse string test

--- testing.test.js
+++ testing.test.js
@@ -1,9 +1,15 @@
-import { sum, capitalize } from './teststing';
+import { sum, capitalize, reverseStr } from './teststing';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+// Capitalized test
 test('returns capitalized string', () => {
   expect(capitalize('anand')).toBe('Anand');
+});
+
+// reverse test
+test('return reversed string', () => {
+  expect(reverseStr('anand')).toBe('dnana');
 });
--- teststing.js
+++ teststing.js
@@ -3,7 +3,11 @@ export function sum(a, b) {
 }
 
 export function capitalize(str) {
-  const part1 = str.toUpperCase().charAt(0);
+  const part1 = str.charAt(0).toUpperCase();
   const part2 = str.slice(1);
   return part1 + part2;
+}
+
+export function reverseStr(str) {
+  return str.split('').reverse().join('');
 }
