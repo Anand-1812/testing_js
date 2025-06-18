82222299eff312b8b0c9aac95aca8766a3a16584
Anand-1812 <anand.work2005@gmail.com>
2025-06-18 16:24:19 +0530

    starting up

---
+++ index.js
@@ -0,0 +1,21 @@
+if (orderTotal ({
+  items : [
+    { name: 'Dragon food', price: 8},
+    { name: 'Dragon cage(small)', price: 800}
+  ]
+}) !== 808) {
+  throw new Error('Check fail: Happy path (Example 1)');
+}
+
+if (orderTotal ({
+  items : [
+    { name: 'Dragon collar', price: 20},
+    { name: 'Dragon chew toy', price: 40}
+  ]
+}) !== 60) {
+  throw new Error('Check fail: Happy path (Example 2)');
+}
+
+function orderTotal(order) {
+  return order.items.reduce((prev, curr) => curr.prev + prev, 0);
+}
