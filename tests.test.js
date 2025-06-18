11196e3ed846e127364a9138ec61a8e1c696e0a2
Anand-1812 <anand.work2005@gmail.com>
2025-06-18 18:18:00 +0530

    rename

---
+++ tests.test.js
@@ -0,0 +1,8 @@
+const { default: test } = require('node:test');
+const { sum } = require('./tests');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('returns capitalized')
